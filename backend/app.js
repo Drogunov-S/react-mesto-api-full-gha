97ae84811880f34a8541250440db0f594662a408
sergey <drogunov-s@yandex.ru>
2023-07-08 11:49:16 +0300

    cors fix all

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,12 @@ const limiter = rateLimit.rateLimit({
   legacyHeaders: false,
 });
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
 app.use(express.json());
 app.use(helmet());
 // app.use(cookieParser());
